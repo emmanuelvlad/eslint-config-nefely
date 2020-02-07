8b5d9095d800738f9c160e2e7cceec695260bbc7
Emmanuel Vlad <emmanuelvlad@gmail.com>
2020-02-07 13:10:23 +0100

    ✨ split into nefely-base and added self linting

--- index.js
+++ index.js
@@ -1,39 +1,6 @@
 module.exports = {
-	extends: ["./rules/vue"].map(require.resolve),
-
-	rules: {
-		"semi": ["error", "always"],
-		"quotes": [2, "double"],
-		"indent": ["error", "tab"],
-		"no-tabs": 0,
-		"no-trailing-spaces": "error",
-		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
-		"no-console": "warn",
-		"no-unused-vars": "warn",
-		"space-unary-ops": [
-			2,
-			{
-				words: true,
-				nonwords: false,
-			},
-		],
-		"space-before-function-paren": [
-			"error",
-			{
-				anonymous: "never",
-				named: "never",
-				asyncArrow: "always",
-			},
-		],
-		"comma-dangle": [
-			"error",
-			{
-				"arrays": "always-multiline",
-				"objects": "always-multiline",
-				"imports": "always-multiline",
-				"exports": "always-multiline",
-				"functions": "never",
-			},
-		],
-	},
+	extends: [
+		"eslint-config-nefely-base",
+		"./rules/vue",
+	].map(require.resolve),
 };
--- rules/vue.js
+++ rules/vue.js
@@ -5,7 +5,7 @@ module.exports = {
 		"sourceType": "module",
 		"ecmaVersion": 2018,
 	},
-	
+
 	plugins: [
 		"vue",
 	],
@@ -16,12 +16,12 @@ module.exports = {
 		"vue/no-multi-spaces": "error",
 
 		"vue/max-attributes-per-line": 0,
-	
+
 		"vue/component-name-in-template-casing": [2, "PascalCase", {
 			"registeredComponentsOnly": true,
 			"ignores": [],
 		}],
-	
+
 		"vue/space-unary-ops": [
 			2,
 			{
@@ -29,30 +29,30 @@ module.exports = {
 				nonwords: false,
 			},
 		],
-	
+
 		"vue/no-use-v-if-with-v-for": ["error", {
 		  	"allowUsingIterationVar": true,
 		}],
-	
+
 		"vue/html-closing-bracket-newline": ["error", {
 		  "singleline": "never",
 		  "multiline": "never",
 		}],
-	
+
 		"vue/max-attributes-per-line": ["error", {
 			"singleline": 1,
 			"multiline": {
 				"max": 1,
 			},
 		}],
-	
+
 		"vue/html-indent": ["error", "tab"],
-	
+
 		"vue/multiline-html-element-content-newline": ["error", {
 			"ignoreWhenEmpty": false,
 			"allowEmptyLines": true,
 		}],
-	
+
 		"vue/multiline-html-element-content-newline": "error",
 	},
 };
